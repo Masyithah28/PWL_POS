9db24985cc835e9e514dd84463a56334a294b217
Masyithah28 <masyithahsophia@gmail.com>
2024-03-06 21:28:03 +0700

    2.2 - Not Found Exceptions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,18 +13,24 @@ class UserController extends Controller
     //
     public function index()
     {
-
-        $user = UserModel::findOr(20, ['username', 'nama'], function () {
-            abort(404);
-        });
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
         
+        // $user = UserModel::findOrFail(1);
+        // return view('user', ['data' => $user]);
+        // $user = UserModel::findOr(20, ['username', 'nama'], function () {
+        //     abort(404);
+        // });
+        //return view('user', ['data' => $user]);
         // $user = UserModel::firstwhere('level_id',1);
         // return view('user', ['data'=> $user]);
         // $user = UserModel::where('level_id',1)->first();
         // return view('user', ['data'=> $user]);
+
         // $user = UserModel::find(1);
-        // return view('user', ['data'=> $user]);
+       // return view('user', ['data' => $user]);
+
+
         // $data = [
         //     'level_id' => 2,
         //     'username' => 'manager_tiga',
