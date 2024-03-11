28eeda40708664190f109450cbfa5900fab8646d
Masyithah28 <masyithahsophia@gmail.com>
2024-03-11 14:00:24 +0700

    Praktikum 2.7 – Relationships

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,10 +8,10 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::all();
+        $user = UserModel::with('level')->get();
         return view('user', ['data' => $user]);
     }
-    
+
     public function tambah()
     {
         return view('user_tambah');
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -13,8 +13,8 @@
 <th>Username</th>
 <th>Nama</th>
 <th>ID Level Pengguna</th>
-{{-- <td>Kode Level</td>
-<td>Nama Level</td> --}}
+<td>Kode Level</td>
+<td>Nama Level</td>
 <th>Aksi</th>
 </tr>
 @foreach ($data as $d)
@@ -23,6 +23,8 @@
     <td>{{ $d->username }}</td>
     <td>{{ $d->nama }}</td>
     <td>{{ $d->level_id }}</td>
+    <td>{{ $d->level->level_kode}}</td>
+    <td>{{ $d->level->level_kode }}</td>
     <td><a href="/user/ubah/{{ $d->user_id}}">Ubah</a> | <a href="/user/hapus/{{ $d->user_id}}">Hapus</a></td>
 @endforeach
 </table>
