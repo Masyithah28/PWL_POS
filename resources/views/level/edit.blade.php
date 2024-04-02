091832e04faf7829c456ebfac4fff2463e7dff2d
Masyithah28 <masyithahsophia@gmail.com>
2024-04-02 14:44:14 +0700

    Tugas Jb 7 -Level User

--- resources/views/level/edit.blade.php
+++ resources/views/level/edit.blade.php
@@ -1,42 +1,57 @@
-@extends('layout.app')
-{{-- Customize layout sections --}}
-@section('subtitle', 'User')
-@section('content_header_title', 'User')
-@section('content_header_subtitle', 'Edit')
-{{-- Content body: main page content --}}
+@extends('layout.template')
+
 @section('content')
-<div class="container">
-    <div class="card card-primary">
-        <div class="card-header">
-        <h3 class="card-title">Edit user</h3>
-        </div>
-        <form method="post" action="{{ route('/user/edit_simpan', $data->user_id)}}">
-            @csrf
-            @method('PUT')
-            <div class='card'></div>
-            <div class="card-body">
-                <div class="form-group">
-                <label for="username">Username</label>
-                <input type="text" class="form-control" id="username" name="username" value="{{$data->username}}">
+<div class="card card-outline card-primary">
+    <div class="card-header">
+        <h3 class="card-title">{{ $page->title }}</h3>
+        <div class="card-tools"></div>
+    </div>
+    <div class="card-body">
+        @empty($level)
+            <div class="alert alert-danger alert-dismissible">
+                <h5><i class="icon fas fa-ban"></i> Kesalahan!</h5>
+                Data yang Anda cari tidak ditemukan.
             </div>
-            <div class="form-group">
-                <label for="nama">Nama</label>
-                <input type="text" class="form-control" id="nama" name="nama" value="{{$data->nama}}">
-            </div>
-            <div class="form-group">
-                <label for="password">Password</label>
-                <input type="text" class="form-control" id="password" name="password" value="{{$data->password}}">
-            </div>
-            <div class="form-group">
-                <label for="level_id">ID Level</label>
-                <input type="text" class="form-control" id="level_id" name="level_id" value="{{$data->level_id}}">
+            <a href="{{ url('level') }}" class="btn btn-sm btn-default mt-2">Kembali</a>
+        @else
+            <form method="POST" action="{{ url('/level/'.$level->level_id) }}" class="form-horizontal">
+                @csrf
+                {!! method_field('PUT') !!} <!-- tambahkan baris ini untuk proses edit yang butuh method PUT -->
+                <div class="form-group row">
+                    <label class="col-1 control-label col-form-label">Kode</label>
+                    <div class="col-11">
+                        <input type="text" class="form-control" id="level_kode" name="level_kode" value="{{ old('level_kode', $level->level_kode )}}" required>
+                        @error('level_kode')
+                            <small class="form-text text-danger">{{ $message }}</small>
+                        @enderror
+                    </div>
                 </div>
-            </div>
-            <div class="card-footer">
-            <a href="{{ route('user.index') }}" class="btn btn-secondary">Back</a>
-            <button type="submit" class="btn btn-primary">Edit</button>
-            </div>
-        </form>
+                <div class="form-group row">
+                    <label class="col-1 control-label col-form-label">Nama</label>
+                    <div class="col-11">
+                        <input type="text" class="form-control" id="level_nama" name="level_nama" value="{{ old('level_nama', $level->level_nama) }}" required>
+                        @error('level_nama')
+                            <small class="form-text text-danger">{{ $message }}</small>
+                        @enderror
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-1 control-label col-form-label"></label>
+                    <div class="col-11">
+                        <button type="submit" class="btn btn-primary btn-sm">Simpan</button>
+                        <a class="btn btn-sm btn-default ml-1" href="{{ url('level') }}">Kembali</a>
+                    </div>
+                </div>
+            </form>
+        @endempty
     </div>
 </div>
 @endsection
+
+@push('css')
+    <!-- Additional CSS -->
+@endpush
+
+@push('js')
+    <!-- Additional JS -->
+@endpush
