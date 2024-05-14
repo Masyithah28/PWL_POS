87c4a580f975532c8dfe7a0ed86cc4df13c24047
Masyithah28 <masyithahsophia@gmail.com>
2024-05-14 21:12:16 +0700

    JB 12 Tugas m_barang

--- app/Models/BarangModel.php
+++ app/Models/BarangModel.php
@@ -5,9 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class BarangModel extends Model
 {
+    use HasFactory;
+
     protected $table = 'm_barang';
     protected $primaryKey = 'barang_id';
     
@@ -16,12 +19,5 @@ class BarangModel extends Model
     public function kategori(): BelongsTo
     {
         return $this->belongsTo(KategoriModel::class, 'kategori_id', 'kategori_id');
-    }
-
-    protected function image(): Attribute
-    {
-        return Attribute::make(
-            get: fn ($image) => url('/storage/posts/' . $image),
-        );
     }
 }
